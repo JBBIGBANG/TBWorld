68abbbf60f91cc61d8c9659b3977ddc2da966fa5
Joy Sarkar <sjoy.cse3.bu@gmail.com>
2022-01-29 22:44:08 +0600

    different bolg type complete

--- src/components/pages/AddPlaces.js
+++ src/components/pages/AddPlaces.js
@@ -5,27 +5,37 @@ import './AddPlaces.css';
 
 const AddPlaces = () => {
     return(
-       
-        <div className='container'>
-            <div className='sub_container'>
-                <div className='text_lebel'>
-                    <label className='lebel_text'>Add Picture & Description</label>
-                </div>
-                <div className='each-div' >   
-                    <input type='file' className='file_upload'></input>
-                </div> 
-                <div>
-                    <textarea className='textarea'>
-                        Description
-                    </textarea>
-                </div>
+       <div className='cardss'>
+            <div className='container'>
+                <div className='sub_container'>
+                    <div className='text_lebel'>
+                        <label className='lebel_text'>Add Picture & Description</label>
+                    </div>
+                    <div className='each-div' >
+                        <div className='radio_formatter'>  
+                            <input type="radio" id="travel" name="fav_language" value="HTML"/>
+                              <label for="html">Travel</label>
+                              <input type="radio" id="travel" name="fav_language" value="CSS"/>
+                              <label for="css">Story</label>
+                              <input type="radio" id="sci-fic" name="fav_language" value="JavaScript"/>
+                              <label for="javascript">Sci-fic</label>
+                        </div>
+                    </div> 
+                    <div className='each-div' >   
+                        <input type='file' className='file_upload'></input>
+                    </div> 
+                    <div>
+                        <textarea className='textarea'>
+                            Description
+                        </textarea>
+                    </div>
 
-                <div className='button_div'>
-                    <button type='submit' className='button' >Submit</button>
+                    <div className='button_div'>
+                        <button type='submit' className='button' >Submit</button>
+                    </div>
                 </div>
             </div>
         </div>
-       
     );
 };
 
